9e4055b1957a7af6c117c744167769016ce11188
kartikayy007 <kartikayy6969@gmail.com>
2025-12-19 15:01:43 +0530

    Initial Commit

---
+++ Pace/PaceApp.swift
@@ -0,0 +1,17 @@
+//
+//  PaceApp.swift
+//  Pace
+//
+//  Created by kartikay on 19/12/25.
+//
+
+import SwiftUI
+
+@main
+struct PaceApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
